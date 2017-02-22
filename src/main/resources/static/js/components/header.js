daf9e4672474bde00430c899e1c50e602114905f
Magnus Jerre <magnus.jerre@ciber.com>
2017-02-22 11:58:04 +0100

    Legger til header

---
+++ src/main/resources/static/js/components/header.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default function Header() {
+    return (
+        <h1>Den store kaffesiden</h1>
+    );
+}
--- src/main/resources/static/js/entry.js
+++ src/main/resources/static/js/entry.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import BryggRegistrering from './components/brygg-registrering';
 import BryggKarakterContainer from './components/brygg-karakter-container.js';
+import Header from './components/header'
 
 ReactDOM.render(
     <div>
+        <Header />
         <BryggRegistrering />
         <BryggKarakterContainer />
     </div>,
