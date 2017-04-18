97da4fc3978520b059ef2b42e492ce5717032ba2
Magnus Jerre <magnus.jerre@gmail.com>
2017-04-18 10:09:25 +0200

    Pynter på BryggRegistreringkomponenten

--- src/main/resources/static/js/components/ContainerComponents/BryggRegistrering.tsx
+++ src/main/resources/static/js/components/ContainerComponents/BryggRegistrering.tsx
@@ -19,18 +19,25 @@ interface IBryggRegistreringComp {
 
 let BryggRegistreringComp : React.StatelessComponent<IBryggRegistreringComp> = ({onChangeBryggNavn, onChangeBrygger, onChangeKaffeId, onChangeLiter, onChangeSkjeer, nyttBrygg, muligeKaffer}) => {
     return (
-        <form>
-            <input name="navn" type="text" onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeBryggNavn(e.currentTarget.value) }} value={nyttBrygg.navn} placeholder="Gang of four"/>
-            <input name="brygger" type="text" onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeBrygger(e.currentTarget.value) }} value={nyttBrygg.brygger} placeholder="Jerre"/>
-            <KaffeSelect id="kaffeSelector" kaffeId={nyttBrygg.kaffeId} name="kaffeSelector" muligeKaffer={muligeKaffer} onChangeKaffe={onChangeKaffeId} />
-            {/*<select name="kaffeId" type="select" onChange={ (e: React.FormEvent<HTMLSelectElement>) => { e.preventDefault(); onChangeKaffeId(e.currentTarget.value)}} value={nyttBrygg.kaffeId}>*/}
-                {/*<option value="def" disabled={true}>Eksempel</option>*/}
-                {/*<option value="cake">Cake</option>*/}
-                {/*<option value="fake">Fake</option>*/}
-            {/*</select>*/}
-            <input name="liter" type="number" min="0.1" step="0.1" value={nyttBrygg.liter} onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeLiter(parseFloat(e.currentTarget.value))}} placeholder="1.0"/>
-            <input name="skjeer" type="number" min="0.25" step="0.25" value={nyttBrygg.skjeer} onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeSkjeer(parseFloat(e.currentTarget.value))}} placeholder="5.5"/>
-            <input name="kaffeid" type="text" onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeKaffeId(e.currentTarget.value) }} value={nyttBrygg.kaffeId} />
+        <form className="holder">
+            <div className="formInputContent">
+                <label htmlFor="navn">Bryggnavn
+                    <input name="navn" type="text" onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeBryggNavn(e.currentTarget.value) }} value={nyttBrygg.navn} placeholder="Gang of four"/>
+                </label>
+                <label htmlFor="brygger">Brygger
+                    <input name="brygger" type="text" onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeBrygger(e.currentTarget.value) }} value={nyttBrygg.brygger} placeholder="Jerre"/>
+                </label>
+                <label htmlFor="kaffeSelector">Kaffe
+                    <KaffeSelect id="kaffeSelector" kaffeId={nyttBrygg.kaffeId} name="kaffeSelector" muligeKaffer={muligeKaffer} onChangeKaffe={onChangeKaffeId} />
+                </label>
+                <label htmlFor="liter">Liter
+                    <input name="liter" type="number" min="0.1" step="0.1" value={nyttBrygg.liter} onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeLiter(parseFloat(e.currentTarget.value))}} placeholder="1.0"/>
+                </label>
+                <label htmlFor="skjeer">Skjeer
+                    <input name="skjeer" type="number" min="0.25" step="0.25" value={nyttBrygg.skjeer} onChange={ (e: React.FormEvent<HTMLInputElement>) => { e.preventDefault(); onChangeSkjeer(parseFloat(e.currentTarget.value))}} placeholder="5.5"/>
+                </label>
+            </div>
+            <input type="submit" value="Registrer brygg"/>
         </form>
     )
 }
